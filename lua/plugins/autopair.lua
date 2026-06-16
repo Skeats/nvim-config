be662a8d38500c58b7bb8d865dbdbae5950e370c
Kiki Mumme <kj.mumme@gmail.com>
2026-06-16 02:42:28 -0600

    Cleaned up autopair plugins + added git plugins

--- lua/plugins/autopair.lua
+++ lua/plugins/autopair.lua
@@ -1,8 +1,32 @@
 -- Automatically adds closing brackets
 
 return {
-  "altermo/ultimate-autopair.nvim",
-  event = { "InsertEnter" },
-  config = true,
-}
+    {
+	"altermo/ultimate-autopair.nvim",
+	event = { "InsertEnter" },
+	config = true,
+    },
+    {
+	"windwp/nvim-ts-autotag",
 
+	config = function()
+	    require('nvim-ts-autotag').setup({
+		opts = {
+		    enable_close = true,
+		    enable_rename = true,
+		    enable_close_on_slash = false,
+		}
+	    })
+	end
+    },
+    {
+	"RRethy/nvim-treesitter-endwise"
+    },
+    {
+	'nvim-mini/mini.surround',
+	version = false,
+	config = function()
+		require("mini.surround").setup()
+	end
+    },
+}
